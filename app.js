674e667db35c21601dc6a1b3c8850bf382ec7959
Mombabil <96351305+Mombabil@users.noreply.github.com>
2026-03-12 14:45:20 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/service-worker.js")
+      .then(() => console.log("Service Worker enregistré"))
+      .catch((err) => console.log("Erreur SW:", err));
+  });
+}
